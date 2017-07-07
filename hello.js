b8600e4b4c69c64884f6dd380b4bd7d8c814629c
Suresh Ramakrishnaiah <sureshram@users.noreply.github.com>
2017-07-07 10:20:10 -0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $.ajax({
-        url: "http://localhost:8080/salesforce-servicecloud-0.1/auth/appConfig/get?activationId=9e4d8f36-e402-4e08-8133-d61f4d294238"
+        url: "https://c3a03051.ngrok.io/salesforce-servicecloud-0.1/auth/appConfig/get?activationId=9e4d8f36-e402-4e08-8133-d61f4d294238"
     }).then(function(data, status, jqxhr) {
        $('.greeting-id').append(data.activationId);
        $('.greeting-content').append(data.companyName);
